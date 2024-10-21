520f185832ef8af63c053fd7a0f350ada54b6231
Daniel Shapero <shapero.daniel@gmail.com>
2024-10-21 11:51:04 -0700

    Oh holy hell it works lol

--- tests/firedrake/simple_shear.py
+++ tests/firedrake/simple_shear.py
@@ -1,4 +1,5 @@
 import firedrake
+from firedrake import inner
 import specfabpy.firedrake.CPO
 
 nx, ny = 16, 16
@@ -12,4 +13,19 @@ cpo = specfabpy.firedrake.CPO.CPO(mesh, (1, 2, 3, 4), 6)
 cpo.initialize()
 for bc_id in (1, 2, 3, 4):
     cpo.set_isotropic_BCs(bc_id)
-cpo.evolve(u, firedrake.as_tensor([[0, 1], [1, 0]]), 0.01)
+
+timestep = 0.01
+final_time = 1.0
+num_steps = int(final_time / timestep)
+for step in range(num_steps):
+    cpo.evolve(u, firedrake.as_tensor([[0, 1], [1, 0]]), timestep)
+
+Q = firedrake.FunctionSpace(mesh, "CG", 1)
+m = firedrake.Function(Q).interpolate(inner(cpo.w, cpo.w))
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+ax.set_aspect("equal")
+colors = firedrake.tripcolor(m, axes=ax)
+fig.colorbar(colors)
+plt.show()
